c3820e9c8b995031d83e45eca8c3d0183218925f
Asim <asim.mahar@gmail.com>
2015-08-05 14:16:44 -0400

    animation & postioning added

--- js/app.js
+++ js/app.js
@@ -3,14 +3,17 @@ $(document).ready(function(){
 $('.ryu').mouseenter(function(){
 	$('.ryu-still').hide();
 	$('.ryu-ready').show();
+	$('.ryu-cool').hide();
 })
 	.mouseleave(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.ryu-cool').hide();
 	})
 	.mousedown(function(){
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
@@ -23,7 +26,30 @@ $('.ryu').mouseenter(function(){
 	.mouseup(function(){
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 	});
+
+$('html').keydown(function(){
+		if (event.which == 88){
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+
+		}
+	});
+
+$('html').keyup(function(){
+	if (event.which == 88){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		$('.ryu-cool').hide();
+
+	}
+});
+
+
+});
+	
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
@@ -33,4 +59,4 @@ function playHadouken () {
 
 
 
-});
+
